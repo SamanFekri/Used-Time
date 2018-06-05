2098aaad1b55f57cb04b6cd853e20a831c8683ef
skings <samanf74@gmail.com>
2018-06-05 19:44:06 +0430

    make login page

--- UI/src/components/Login.vue
+++ UI/src/components/Login.vue
@@ -2,20 +2,29 @@
   <v-app>
     <v-container grid-list-xl text-xs-center>
       <v-layout row wrap>
-        <v-flex xs10 offset-xs1>
+        <v-flex xs6 offset-xs3>
           <v-card color="white" class="pa-5">
-            <v-form ref="form" lazy-validation>
+            <v-form ref="form">
+              <v-text-field v-bind="name" label="Name" type="text" prepend-icon="account_circle" single-line required/>
               <v-text-field
-                :counter="10"
-                label="Name"
-                required>
-              </v-text-field>
+                v-bind="password"
+                label="Password"
+                :type="can_see ? 'text': 'password'"
+                prepend-icon="lock"
+                :append-icon="can_see ? 'visibility' : 'visibility_off'"
+                :append-icon-cb="() => (can_see = !can_see)"
+                single-line required/>
               <v-text-field
-                label="E-mail"
-                required>
-              </v-text-field>
-
-              <v-btn color="success">submit</v-btn>
+                v-show="is_register"
+                v-bind="repassword"
+                label="Re-Enter"
+                :type="can_see ? 'text': 'password'"
+                prepend-icon="lock"
+                :append-icon="can_see ? 'visibility' : 'visibility_off'"
+                :append-icon-cb="() => (can_see = !can_see)"
+                single-line required/>
+              <p @click="toggleSignin" class="text-xs-left ml-1 hover">{{msg}}</p>
+              <v-btn color="success">{{btn_txt}}</v-btn>
             </v-form>
           </v-card>
         </v-flex>
@@ -24,8 +33,33 @@
   </v-app>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: 'login-page',
+  data () {
+    return {
+      name: '',
+      password: '',
+      repassword: '',
+      msg: 'I don`t have an account',
+      btn_txt: 'Sing in',
+      can_see: false,
+      is_register: false
+    }
+  },
+  methods: {
+    toggleSignin () {
+      this.is_register = !this.is_register
+      this.msg = this.is_register ? 'I`m already a member' : 'I don`t have an account'
+      this.btn_txt = this.is_register ? 'Sign up' : 'Sign in'
+    }
+  }
+}
+</script>
 
 <style scoped>
-
+.hover:hover{
+  color: #1470aa;
+  cursor: pointer;
+}
 </style>
